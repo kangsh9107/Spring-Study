2db967781833276259cd16033b21c53779ec4077
kangsh9107 <kangsh9107@naver.com>
2022-11-19 18:17:03 +0900

    study

---
+++ kang-spring/src/main/java/kang/kangspring/KangSpringApplication.java
@@ -0,0 +1,14 @@
+package kang.kangspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KangSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(KangSpringApplication.class, args);
+	}
+
+}
---
+++ kang-spring/src/test/java/kang/kangspring/KangSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package kang.kangspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KangSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
