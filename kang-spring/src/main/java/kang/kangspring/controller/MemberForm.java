9642716f3790f8ff80e2dda5b238458c31733c63
kangsh9107 <kangsh9107@naver.com>
2022-11-27 19:07:42 +0900

    study

---
+++ kang-spring/src/main/java/kang/kangspring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package kang.kangspring.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ kang-spring/src/main/java/kang/kangspring/repository/MemoryMemberRepository.java
@@ -0,0 +1,41 @@
+package kang.kangspring.repository;
+
+import kang.kangspring.domain.Member;
+
+import java.util.*;
+
+/**
+ * 간단한 예제이므로 동시성 문제를 고려하지 않았음.
+ * 실무에서는 동시성 문제를 고려해서 ConcurrentHashMap, AtomicLong을 사용함.
+ */
+
+public class MemoryMemberRepository implements MemberRepository {
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
